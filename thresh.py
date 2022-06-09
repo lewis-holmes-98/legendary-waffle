9546d3abea2e0ef8041bf0f3a26f7534715d93c5
lewis-holmes-98 <4holml09@solent.ac.uk>
2022-06-09 17:11:09 +0100

    complete further threshold code.

---
+++ thresh.py
@@ -0,0 +1,51 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Video timestamp: 2:15:22
+
+Convert and image into binary:
+0 = black
+255 = white
+
+"""
+
+# Read image:
+base_img = cv.imread('photos/bonsai.jpg')
+
+
+def rescaleFrame(frame, scale=0.40):
+    # Rescale size by 50%
+    # Images, Videos and Live Video(e.g. webcams)
+
+    width = int(frame.shape[1] * scale // 0.8)
+    height = int(frame.shape[1] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+
+# Original image
+img = rescaleFrame(base_img)
+cv.imshow('Original Image', img)
+
+# Convert to grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# Simple Thresholding
+# compares pixels and assigns value based on threshold (0, 255)
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+cv.imshow('Simple Threshold', thresh)
+
+# Inverse Threshold
+threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
+cv.imshow('Simple Threshold Inverse', thresh_inv)
+
+
+# Adaptive Thresholding
+# Automatically define optimal threshold value
+adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 3)
+cv.imshow('Adaptive Threshold', adaptive_thresh)
+
+cv.waitKey(0)
