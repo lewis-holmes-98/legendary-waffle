a7b773d8d5697d692d7b2954d137846ebae5cd10
lewis-holmes-98 <4holml09@solent.ac.uk>
2022-06-09 11:27:07 +0100

    complete colour channel splitting code.

--- splitmerge.py
+++ splitmerge.py
@@ -1,4 +1,4 @@
-import cv2.cv2 as cv
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -25,6 +25,31 @@ def rescaleFrame(frame, scale=0.40):
 
 # Original image
 img = rescaleFrame(base_img)
-cv.imshow('Original', img)
+
+# Create blank image for demonstration of individual colour channels
+blank = np.zeros(img.shape[:2], dtype='uint8')
 
 b, g, r = cv.split(img)
+
+# 'blank' sets colour channel to black, only showing one colour
+blue = cv.merge([b, blank, blank])
+green = cv.merge([blank, g, blank])
+red = cv.merge([blank, blank, r])
+
+cv.imshow('Original Image', img)
+cv.imshow('Blue', blue)
+cv.imshow('Green', green)
+cv.imshow('Red', red)
+
+# Visualise shape of image
+print(img.shape)
+print(b.shape)
+print(g.shape)
+print(r.shape)
+
+# Merge colour channels
+merged = cv.merge([b, g, r])
+cv.imshow('Merged Image', merged)
+
+cv.waitKey(0)
+
